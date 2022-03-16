7076261a1c33e7530971cc08b5736fc1fad46f6d
Steven Kravitz <Steven.Kravitz95@gmail.com>
2022-03-16 00:53:34 -0400

    wrote get, post, put, and delete routes for posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,3 +1,129 @@
-const router = require('express').Router();
+const router = require('express').Router()
 const {Post, User, Comment} = require('../../models')
-const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth')
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        
+        attributes: ['id', 'title', 'post', 'user_id', 'created_at'],
+        order: [['created_at', 'ASC']],
+        
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+      
+    }).then(postDbData => res.json(postDbData))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+})
+
+router.get('/:id', (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        
+        attributes: ['id', 'title', 'post', 'created_at'],
+        
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+            model: Comment,
+            attributes: ['id', 'comment', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+    ]
+    }).then(postDbData => {
+        if(!postDbData) {
+            res.status(404).json({ message: 'No post associated with this id!' })
+            return
+        }
+        res.json(postDbData)
+
+    }) .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post: req.body.post,
+        user_id: req.session.user_id
+    }) .then(postDbData => res.json(postDbData))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+})
+
+router.post('/', withAuth, async (req, res) => {
+    const body = req.body
+  
+    try {
+      const newPost = await Post.create({ ...body, user_id: req.session.user_id })
+      res.json(newPost)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  })
+router.put('/:id', withAuth, (req, res) => {
+    Post.update({
+        title: req.body.title,
+        post: req.body.post
+    },
+    {
+        where: {
+            id: req.params.id
+        }
+    }).then (postDbData => {
+        if(!postDbData) {
+            res.status(404).json({ message: 'No post associated with this id! Please try another id.' })
+            return
+        } res.json(postDbData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })    
+})
+
+  
+  router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    }) .then (postDbData => {
+        if(!postDbData) {
+            res.status(404).json({ message: 'No post associated with this id! Please double check the id.' })
+            return
+        } res.json(postDbData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })    
+})
+
+module.exports = router
